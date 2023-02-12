0875b7feb1b50cbd5562d5b1132a9b7cbe8a11ac
kulmboin <dltjrwn2000@gmail.com>
2023-02-13 00:26:29 +0900

    base - 0.4

---
+++ app.js
@@ -0,0 +1,16 @@
+import CreateCard from "./component/weatherCard.js";
+
+const $element = document.querySelector(".weather_day_box");
+
+CreateCard($element);
+
+//   자바스크립트로 추가하기
+
+//   <li class="weather_card">
+//     <div class="dayInfo_section">
+//       <span class="day_data">월</span>
+//       <span class="date_data"></span>
+//     </div>
+//     <div class="morning_section"></div>
+//     <div class="afternoon_section"></div>
+//   </li>
